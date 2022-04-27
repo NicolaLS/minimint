23fa7a68eb49c4efcc37a162689c25d6b547a7e4
nicolasusca <nicolaleonardo.susca@gmail.com>
2022-04-27 15:43:53 +0200

    hsjkfmbdbjv

--- scripts/integrationtest.sh
+++ scripts/integrationtest.sh
@@ -141,10 +141,10 @@ sleep 15
 #JSON-RPC Specification
 #TODO: Notification: no ID means the client does not want any response (reissue for example)
 #rpc call of non-existent method:
-#RES=$(curl -X POST $RPC -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0","method": "doesnotexist","params": null,"id": 1}');
-#RES_ERROR=$(echo $RES | jq '.error');  #should contain an rpc error -32601
-#RES_RES=$(echo  $RES | jq '.result'); #should be null
-#[[ $RES_RES = "null" ]]
+RES=$(curl -X POST $RPC -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0","method": "doesnotexist","params": null,"id": 1}');
+RES_ERROR=$(echo $RES | jq '.error');  #should contain an rpc error -32601
+RES_RES=$(echo  $RES | jq '.result'); #should be null
+[[ $RES_RES = "null" ]]
 #[[ $(echo $RES_ERROR | jq '.code') = "-32601" ]]
 #rpc call with invalid JSON:
 #RES=$(curl -X POST $RPC -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0","info": "info","params": null,"id": 1'); # the '}' is missing
